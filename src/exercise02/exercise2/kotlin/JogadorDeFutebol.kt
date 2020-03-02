c423be49dfcd7aa4404e2193b4ef24389df7536a
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-02 02:23:20 -0300

    Primeira Lista de Exercícios - Exercicio 2: Jogadores e treinamento em Kotlin adicionado

---
+++ src/exercise02/exercise2/kotlin/JogadorDeFutebol.kt
@@ -0,0 +1,17 @@
+package exercise02.exercise2.kotlin
+
+class JogadorDeFutebol(var nome: String, var energia: Int, var alegria: Int, var gols: Int, var experiencia: Int) {
+
+    fun fazerGol() {
+        energia -= 5
+        alegria += 10
+        gols++
+        println("GOOOOL!")
+    }
+
+    fun correr() {
+        energia -= 10
+        println("Cansei")
+    }
+
+}
