336a32949454dbbb1e5d2cf909a1455837c22e35
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-02 03:21:59 -0300

    Primeira Lista de Exercícios - Exercício 3: Atletas e prova em Java adicionado

---
+++ src/exercise02/exercise3/java/Prova.java
@@ -0,0 +1,32 @@
+package exercise02.exercise3.java;
+
+public class Prova {
+    private int dificuldade;
+    private int energiaNecessaria;
+
+    public Prova(int dificuldade, int energiaNecessaria) {
+        this.dificuldade = dificuldade;
+        this.energiaNecessaria = energiaNecessaria;
+    }
+
+    public boolean podeRealizar(Atleta atleta){
+        if (atleta.getNivel() >= getDificuldade() && atleta.getEnergia() >= getEnergiaNecessaria()) return true;
+        else return false;
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public int getEnergiaNecessaria() {
+        return energiaNecessaria;
+    }
+
+    public void setEnergiaNecessaria(int energiaNecessaria) {
+        this.energiaNecessaria = energiaNecessaria;
+    }
+}
