58a42d3df36aad1232169566dfb6e2ea13cf3e46
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-06 10:02:06 -0300

    Primeira Tarefa - Exercicio 1 - codigos em Java adicionados!

---
+++ src/exercise04/tarefa/tarefa01/java/Cliente.java
@@ -0,0 +1,24 @@
+package exercise04.tarefa.tarefa01.java;
+
+public class Cliente extends Pessoa {
+    private int codigo;
+
+    public Cliente(int codigo, String nome, Data nascimento) {
+        super(nome, nascimento);
+        this.codigo = codigo;
+    }
+
+    @Override
+    public void imprimeDados() {
+        System.out.println("Código do Cliente: " + codigo);
+        super.imprimeDados();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ src/exercise04/tarefa/tarefa01/java/TestaCadastroMain.java
@@ -0,0 +1,18 @@
+package exercise04.tarefa.tarefa01.java;
+
+public class TestaCadastroMain {
+    public static void main(String[] args) {
+
+        CadastroPessoas cadastro1 = new CadastroPessoas();
+
+        Cliente clienteRobson = new Cliente(1, "Robson", new Data(12,12,1995));
+        Funcionario funcionarioRobson = new Funcionario("Robson", new Data(10,5,1989), 3000.0f);
+        Gerente gerenteRobson = new Gerente("Robson", new Data(27,7,1973), 9000.0f, "TI");
+
+        cadastro1.cadastraPessoa(clienteRobson);
+        cadastro1.cadastraPessoa(funcionarioRobson);
+        cadastro1.cadastraPessoa(gerenteRobson);
+        cadastro1.getQtdAtual();
+        cadastro1.imprimeCadastro();
+    }
+}
