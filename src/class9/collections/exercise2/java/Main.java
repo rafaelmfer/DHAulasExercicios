f87a6e716170f1fc31e6ee8e7b59a47c479a7feb
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-12 18:08:55 -0300

    Lista de Exercicios Collections (Dicionario) - Exercicio 2 em Java e Kotlin Adicionados

---
+++ src/class9/collections/exercise2/java/Main.java
@@ -0,0 +1,42 @@
+package class9.collections.exercise2.java;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        createList();
+        createHashSet();
+    }
+
+    private static void createList() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("5");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("8");
+        list.add("8");
+
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+
+    private static void createHashSet() {
+        HashSet<String> conjunto = new HashSet<>();
+        conjunto.add("1");
+        conjunto.add("5");
+//        conjunto.add("5");    Não permite valores duplicados
+        conjunto.add("6");
+        conjunto.add("7");
+        conjunto.add("8");
+
+        for (String valor : conjunto) {
+            System.out.println(valor);
+        }
+    }
+}
