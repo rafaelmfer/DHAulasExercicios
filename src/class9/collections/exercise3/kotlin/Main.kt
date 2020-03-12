26a395cbfd52b69caa42d0a31621afe5a2914658
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-12 18:19:21 -0300

    Lista de Exercicios Collections (Dicionario) - Exercicio 3 em Java e Kotlin Adicionados

---
+++ src/class9/collections/exercise3/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package class9.collections.exercise3.kotlin
+
+import java.util.HashSet
+
+fun main(args: Array<String>) {
+    Prova().somaTotal(HashSet<Int>().apply {
+        add(1)
+        add(5)
+        add(6)
+        add(7)
+        add(8)
+        add(9)
+        add(10)
+        add(4)
+        add(2)
+        add(3)
+    })
+}
