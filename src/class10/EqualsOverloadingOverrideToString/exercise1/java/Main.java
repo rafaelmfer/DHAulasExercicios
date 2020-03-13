9151c1bab8e01733a4f079fcff8e00966bfab191
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-12 21:07:49 -0300

    Lista de Exercicios Equals, Overloading, Override, toString ---- Exercicios 1 e 2 em Java adicionado (Porque são muito parecidos)

---
+++ src/class10/EqualsOverloadingOverrideToString/exercise1/java/Main.java
@@ -0,0 +1,10 @@
+package class10.EqualsOverloadingOverrideToString.exercise1.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoaRafael = new Pessoa("Rafael", 456578952);
+        Pessoa pessoaDanny = new Pessoa("Danny", 456578952);
+
+        System.out.println(pessoaRafael.equals(pessoaDanny));
+    }
+}
