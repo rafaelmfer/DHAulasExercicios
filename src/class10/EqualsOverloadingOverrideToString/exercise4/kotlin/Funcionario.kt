a7598d5321ffb807bbaf2871ab2a732c28ec9430
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-13 02:37:19 -0300

    Lista de Exercicios Equals, Overloading, Override, toString ---- Exercicios 3 e 4 em kotlin adicionado (Porque são muito parecidos)

---
+++ src/class10/EqualsOverloadingOverrideToString/exercise4/kotlin/Funcionario.kt
@@ -0,0 +1,23 @@
+package class10.equalsoverloadingoverridetostring.exercise4.kotlin
+
+class Funcionario(var nome: String, var numeroDeRegistro: Int) {
+
+    //Equals padrão que compara o objeto inteiro
+//    override fun equals(other: Any?): Boolean {
+//        if (this === other) return true
+//        if (javaClass != other?.javaClass) return false
+//        other as Funcionario
+//        if (nome != other.nome) return false
+//        if (numeroDeRegistro != other.numeroDeRegistro) return false
+//        return true
+//    }
+
+    //Equals que compara apenas o Numero de Registro do objeto
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Funcionario
+        if (numeroDeRegistro != other.numeroDeRegistro) return false
+        return true
+    }
+}
