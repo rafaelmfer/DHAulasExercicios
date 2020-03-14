f154a7f9e6d346179519c58c383a64f0c665dee9
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-14 12:17:18 -0300

    Exercicio Integrador ---- Exercicio sem o Desafio em Java adicionado

---
+++ src/class12/integratingexercise/java/Book.java
@@ -0,0 +1,87 @@
+package class12.integratingexercise.java;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int releaseYear;
+    private int codeISBN;
+    private int stockQuantity;
+    private Double price;
+
+    public Book(int id, String title, String author, int releaseYear, int codeISBN, int stockQuantity, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.releaseYear = releaseYear;
+        this.codeISBN = codeISBN;
+        this.stockQuantity = stockQuantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getCodeISBN() {
+        return codeISBN;
+    }
+
+    public void setCodeISBN(int codeISBN) {
+        this.codeISBN = codeISBN;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro {codigo = " + id +
+                ", titulo = '" + title +
+                '\'' + ", autor = '" + author +
+                '\'' + ", anoDeLancamento = " + releaseYear +
+                ", Estoque = " + stockQuantity +
+                ", preco = " + price + '}';
+    }
+}
