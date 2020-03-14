44597ebcdd2d694407528c7546c47f6ee80a1400
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-03-14 08:54:05 -0300

    Tarefa Aula 7 Interfaces ---- Tarefa em Java adicionado

---
+++ src/class7/interfaces/tarefa/java/OperacaoMatematica.java
@@ -0,0 +1,6 @@
+package class7.interfaces.tarefa.java;
+
+public interface OperacaoMatematica {
+
+    int calcula(int a, int b);
+}
---
+++ src/class7/interfaces/tarefa/java/Soma.java
@@ -0,0 +1,10 @@
+package class7.interfaces.tarefa.java;
+
+public class Soma implements OperacaoMatematica {
+    @Override
+    public int calcula(int a, int b) {
+        int soma = a + b;
+        System.out.println("Soma = " + soma);
+        return soma;
+    }
+}
