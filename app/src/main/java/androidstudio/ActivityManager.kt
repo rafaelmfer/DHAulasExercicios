f372a720414a4dabcf6f81a92d603dc766723f4d
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-01 19:24:32 -0300

    Ajuste na ActivityManager - inclusão de uma recycler view para melhor visualização das atividades.

--- app/src/main/java/androidstudio/ActivityManager.kt
+++ app/src/main/java/androidstudio/ActivityManager.kt
@@ -2,6 +2,11 @@ package androidstudio
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidstudio.class18.java.ProductWithDiscountActivity
 import androidstudio.class18.kotlin.ProductWithDiscountAct
 import androidstudio.class20.java.RegisterActivity
@@ -11,38 +16,86 @@ import androidstudio.class21.kotlin.TelephoneBookAct
 import androidstudio.class23.java.Class23Activity
 import androidstudio.class23.kotlin.Class23Act
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.rafaelmfer.codeclasses.R
 import kotlinx.android.synthetic.main.activity_manager.*
+import kotlin.reflect.KClass
 
 class ActivityManager : AppCompatActivity() {
+
+    data class ClassExercises(
+        var title: String,
+        var javaActivity: KClass<out AppCompatActivity>,
+        var kotlinActivity: KClass<out AppCompatActivity>
+    )
+
+    var classExercises = listOf(
+        ClassExercises(
+            title = "AULA 18 - Layouts",
+            javaActivity = ProductWithDiscountActivity::class,
+            kotlinActivity = ProductWithDiscountAct::class
+        ),
+        ClassExercises(
+            title = "AULA 20 - Material Design Especialista e Componentes",
+            javaActivity = RegisterActivity::class,
+            kotlinActivity = RegisterAct::class
+        ),
+        ClassExercises(
+            title = "AULA 21 - Comunicação Entre Activitys",
+            javaActivity = TelephoneBookActivity::class,
+            kotlinActivity = TelephoneBookAct::class
+        ),
+        ClassExercises(
+            title = "AULA 23 - Fragments",
+            javaActivity = Class23Activity::class,
+            kotlinActivity = Class23Act::class
+        )
+//        ClassExercises(title = "AULA 24 - Comunicação Entre Fragments",)
+//        ClassExercises(title = "AULA 26 - NavigationView e DrawerLayout",)
+//        ClassExercises(title = "AULA 27 - RecyclerView",)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manager)
 
-        btJavaClass18.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, ProductWithDiscountActivity::class.java))
+        rvActsList.apply {
+            layoutManager = LinearLayoutManager(this@ActivityManager)
+            adapter = ActsListAdapter(classExercises)
         }
-        btKotlinClass18.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, ProductWithDiscountAct::class.java))
+
+    }
+}
+
+class ActsListAdapter(var classExercises: List<ActivityManager.ClassExercises>) : RecyclerView.Adapter<ActsListAdapter.ActsListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ActsListViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_acts_list, parent, false)
+    )
+
+    override fun getItemCount() = classExercises.size
+
+    override fun onBindViewHolder(holder: ActsListViewHolder, position: Int) {
+        classExercises[position].run {
+
+            holder.apply {
+                tvTitleClassName.text = title
+
+                btJava.setOnClickListener {
+                    it.context.startActivity(Intent(it.context, javaActivity.java))
+                }
+
+                btKotlin.setOnClickListener {
+                    it.context.startActivity(Intent(it.context, kotlinActivity.java))
+                }
+            }
         }
-        btJavaClass20.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, RegisterActivity::class.java))
-        }
-        btKotlinClass20.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, RegisterAct::class.java))
-        }
-        btJavaClass21.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, TelephoneBookActivity::class.java))
-        }
-        btKotlinClass21.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, TelephoneBookAct::class.java))
-        }
-        btJavaClass23.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, Class23Activity::class.java))
-        }
-        btKotlinClass23.setOnClickListener {
-            startActivity(Intent(this@ActivityManager, Class23Act::class.java))
-        }
+    }
+
+    class ActsListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitleClassName: TextView = itemView.findViewById(R.id.tvTitleClassName)
+        val btJava: Button = itemView.findViewById(R.id.btJava)
+        val btKotlin: Button = itemView.findViewById(R.id.btKotlin)
     }
 }
