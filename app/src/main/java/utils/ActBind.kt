ea47e822f0184ee5ef089003a77785d12a705bd8
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-09 00:48:00 -0300

    Configuraçao para facilitar a ViewBinding

---
+++ app/src/main/java/utils/ActBind.kt
@@ -0,0 +1,26 @@
+package utils
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+
+abstract class ActBind<T : ViewBinding>(private val clazz: Class<T>) : ActBase() {
+
+    lateinit var binding: T
+
+    @Suppress("UNCHECKED_CAST")
+    fun inflate() =
+        clazz.getMethod("inflate", LayoutInflater::class.java)
+            .invoke(null, layoutInflater) as T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflate()
+        setContentView(binding.root)
+        binding.onBoundView()
+    }
+
+    abstract fun T.onBoundView()
+}
+
+
