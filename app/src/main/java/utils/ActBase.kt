c90f2a4a61b5fa942f0f95b30e38ca0ecaa4ef34
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-09 00:03:04 -0300

    ActBase

---
+++ app/src/main/java/utils/ActBase.kt
@@ -0,0 +1,43 @@
+package utils
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+
+const val teste = "teste"
+
+open class ActBase(val layout: Any? = null) : AppCompatActivity() {
+
+    companion object {
+        const val teste = "string"
+
+        @JvmStatic lateinit var currentActivity: AppCompatActivity
+
+    }
+
+    open var viewModel: ViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (layout) {
+            is Int -> setContentView(layout)
+            is View -> setContentView(layout)
+        }
+        intent?.extras?.onExtras()
+        viewModel?.onViewModel()
+        onView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        currentActivity = this
+    }
+
+    open fun Bundle.onExtras() {}
+
+    open fun ViewModel.onViewModel() {}
+
+    open fun onView() {}
+
+}
