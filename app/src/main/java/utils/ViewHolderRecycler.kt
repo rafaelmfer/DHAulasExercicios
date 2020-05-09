9d1be70cde104c4f8c21e6e195d83a3884adb4e6
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-09 00:39:35 -0300

    Extensions functions de RecyclerViewBuilder e ViewHolderRecycler para facilitar a criação de recyclers

---
+++ app/src/main/java/utils/ViewHolderRecycler.kt
@@ -0,0 +1,42 @@
+package utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+open class RecyclerViewHolder(val builder: RecyclerViewBuilder<*, *>) :
+    RecyclerView.ViewHolder(builder.build())
+
+abstract class RecyclerViewBuilder<Data, Binding : ViewBinding> {
+
+    abstract val bindClass: Class<Binding>
+    private lateinit var binding: Binding
+    lateinit var collection: Collection<Data>
+
+    private lateinit var viewGroup: ViewGroup
+
+    @Suppress("UNCHECKED_CAST")
+    fun init(viewGroup: ViewGroup, collection: Collection<*>): RecyclerViewBuilder<Data, Binding> {
+        this.viewGroup = viewGroup
+        this.collection = collection as Collection<Data>
+        return this
+    }
+
+    fun build(): View {
+        binding = inflate()
+        return binding.root
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun inflate() =
+        bindClass.getMethod("inflate", LayoutInflater::class.java).invoke(
+            null, ((viewGroup.context) as AppCompatActivity).layoutInflater
+        ) as Binding
+
+    fun bind(position: Int) = binding.onBind(position)
+
+    abstract fun Binding.onBind(position: Int)
+}
